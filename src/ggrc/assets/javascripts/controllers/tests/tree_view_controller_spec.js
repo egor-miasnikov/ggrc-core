592c3ccc672a1a6e5cf911e00eea9c8e26bde75d
Alexander Novik <al.novik88@gmail.com>
2016-05-25 12:28:10 +0300

    Add tests for VeiwController

--- src/ggrc/assets/javascripts/controllers/tests/tree_view_controller_spec.js
+++ src/ggrc/assets/javascripts/controllers/tests/tree_view_controller_spec.js
@@ -42,4 +42,80 @@ describe('CMS.Controllers.TreeView', function () {
 
     // test cases here...
   });
+
+  describe('_build_request_params() method', function () {
+    var ctrlInst;  // fake controller instance
+    var method;
+
+    beforeEach(function () {
+
+      ctrlInst = {
+        options: new can.Map({
+          paging: {
+            current: 1,
+            total: null,
+            page_size: 10,
+            count: 6
+          }
+        })
+      };
+
+      method = Ctrl.prototype._build_request_params.bind(ctrlInst);
+    });
+
+    it('return default params for paging request', function () {
+      var expected = {
+        __page: 1,
+        __page_size: 10
+      };
+
+      expect(method()).toEqual(expected);
+    });
+
+    it('return params for paging request', function () {
+      var expected = {
+        __page: 5,
+        __page_size: 25
+      };
+
+      ctrlInst.options.paging.attr('current', 5);
+      ctrlInst.options.paging.attr('total', 150);
+      ctrlInst.options.paging.attr('page_size', 25);
+
+      expect(method()).toEqual(expected);
+    });
+  });
+
+  describe('find() method', function () {
+    var ctrlInst;  // fake controller instance
+    var method;
+    var $element;
+
+    beforeEach(function () {
+
+      $element = $('<div><div class="cms_controllers_tree_view_node"></div></div>');
+
+      ctrlInst = {
+        element: $element,
+        _build_request_params: jasmine.createSpy('_build_request_params'),
+        enqueue_items: jasmine.createSpy('enqueue_items'),
+        options: new can.Map({
+          model: {
+            findAll: null
+          }
+        })
+      };
+
+      method = Ctrl.prototype.find.bind(ctrlInst);
+    });
+
+    it('find()', function () {
+
+      spyOn(ctrlInst.options.model, "findAll").and.returnValue(new $.Deferred().resolve([]));
+
+      method();
+      expect(ctrlInst.enqueue_items).toHaveBeenCalled();
+    });
+
+  });
 });
